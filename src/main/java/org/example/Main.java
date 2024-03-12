b7c53af27803415d141caa6157cef94c3e0d281e
Aynur Sirinova <wirinovaaynur123@gmail.com>
2024-03-12 15:23:28 +0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.controller.ProjectController;
 import org.example.controller.TodoController;
+import org.example.controller.UserController;
 import org.example.repository.ProjectRepository;
 import org.example.repository.TodoRepository;
 import org.example.service.ProjectService;
@@ -17,10 +19,11 @@ public class Main {
     TodoService todoService = new TodoService(repository,userService);
     public TodoController todoController = new TodoController(todoService);
     public ProjectService projectService = new ProjectService(projectRepository, todoService);
-
+   public ProjectController projectController = new ProjectController(projectService, projectRepository);
+   public UserController userController = new UserController(userService);
 
     public static void main(String[] args) {
-        new Main().userService.loging();
+        new Main().userController.loging();
 
 
 
