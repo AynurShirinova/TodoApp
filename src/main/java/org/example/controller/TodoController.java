78c67dda677bd110f3a4c39d15dd1710c0a94448
Aynur Sirinova <wirinovaaynur123@gmail.com>
2024-03-30 03:01:03 +0400

    Update TodoController.java

--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -1,9 +1,10 @@
 package org.example.controller;
+import org.example.domain.Status;
 import org.example.domain.Todo;
 import org.example.service.TodoService;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
@@ -34,7 +35,7 @@ public class TodoController {
 
                 switch (choice) {
                     case "1":
-                        todoService.addTask();
+                        addTask();
                         break;
                     case "2":
                         deleteTask();
@@ -43,7 +44,7 @@ public class TodoController {
                         updateTask();
                         break;
                     case "4":
-                        todoService.readTasks();
+                        readTasks();
                         break;
                     case "5":
                         listTasksByAssignedTo();
@@ -61,60 +62,151 @@ public class TodoController {
             } while (!choice.equals("0"));
         }
 
-    private void askUserForDateRangeAndPrintTodos() {
-        System.out.println("Enter start date (dd/MM/yyyy):");
-        String startDateInput = scanner.nextLine();
-        LocalDate startDate = LocalDate.parse(startDateInput, dateFormatter);
-
-        System.out.println("Enter end date (dd/MM/yyyy):");
-        String endDateInput = scanner.nextLine();
-        LocalDate endDate = LocalDate.parse(endDateInput, dateFormatter);
-      //  List<Todo> filteredTodos = todoService.askUserForDateRangeAndPrintTodos(startDate, endDate);
-    //    filteredTodos.forEach(task -> System.out.println(task));
-        Todo todo = Todo.builder()
-                .endDateInput(endDateInput)
-                .startDateInput(startDateInput)
-                .build();
-        todoService.askUserForDateRangeAndPrintTodos(todo);
-    }
-
     private void listTasksByAssignedTo() {
-        System.out.println("Enter the ID of the user to list tasks for:");
-        String userIdString = scanner.nextLine();
-        UUID userId = UUID.fromString(userIdString);
-        Todo todo = Todo.builder()
-                .id(userId)
-                .build();
-        List<Todo> assignedTasks = todoService.printTodos(todo);
-        if (assignedTasks.isEmpty()) {
-            System.out.println("No tasks assigned to user with ID " + userId);
-        } else {
-            System.out.println("Tasks assigned to user with ID " + userId + ":");
-            assignedTasks.forEach(task -> System.out.println(task));
+        try {
+            System.out.println("Enter the ID of the user to list tasks for:");
+            String userIdString = scanner.nextLine();
+            UUID userId = UUID.fromString(userIdString);
+            List<Todo> assignedTasks = todoService.listTasksByAssignedTo(userId);
+            if (assignedTasks.isEmpty()) {
+                System.out.println("No tasks assigned to user with ID " + userId);
+            } else {
+                System.out.println("Tasks assigned to user with ID " + userId + ":");
+                printTodos(assignedTasks);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid user ID format.");
         }
     }
 
 
-    private void updateTask() {
-        System.out.println("Enter the ID of the task you want to update:");
-        String idString = scanner.nextLine();
-        UUID id = UUID.fromString(idString);
-        Todo todo= Todo.builder()
-                .id(id)
-                .build();
-        todoService.updateTask(todo);
+    private void readTasks() {
+            List<Todo> tasks = todoService.readTasks();
+            if (tasks.isEmpty()) {
+                System.out.println("No tasks found.");
+            } else {
+                System.out.println("Tasks:");
+                for (Todo task : tasks) {
+                    System.out.println(task);
+                }
+            }
+        }
+
+
+
+    private void addTask() {
+        try {
+            System.out.println("Enter the ID of the user to assign the task:");
+            String userIdString = scanner.nextLine();
+            UUID assignedTo = UUID.fromString(userIdString);
+
+            System.out.println("Enter title:");
+            String title = scanner.nextLine();
+
+            System.out.println("Enter description:");
+            String description = scanner.nextLine();
+
+            System.out.println("Enter status:");
+            String status = scanner.nextLine();
+
+            System.out.println("Enter start date (dd/MM/yyyy):");
+            String startDateInput = scanner.nextLine();
+            LocalDate startDate = LocalDate.parse(startDateInput, dateFormatter);
+
+            Todo newTodo = Todo.builder()
+                    .assignedTo(assignedTo)
+                    .title(title)
+                    .description(description)
+                    .status(Status.valueOf(status))
+                    .created(startDate)
+                    .build();
+
+            todoService.addTask(newTodo);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid date format. Please enter date in dd/MM/yyyy format.");
+        }
+    }
+
+    private void askUserForDateRangeAndPrintTodos() {
+        try {
+            System.out.println("Enter start date (dd/MM/yyyy):");
+
+            String startDateInput = scanner.nextLine();
+            LocalDate startDate = LocalDate.parse(startDateInput, dateFormatter);
+
+            System.out.println("Enter end date (dd/MM/yyyy):");
+            String endDateInput = scanner.nextLine();
+            LocalDate endDate = LocalDate.parse(endDateInput, dateFormatter);
+            List<Todo> filteredTodos = todoService.getTodosBetweenDates(startDate, endDate);
+            printTodos(filteredTodos);
+        } catch (Exception e) {
+            System.out.println("Invalid date format. Please enter date in dd/MM/yyyy format.");
+        }
 
     }
 
-    private void deleteTask() {
-            System.out.println("Enter the ID of the task you want to delete:");
-            String idString = scanner.nextLine();
-            UUID id = UUID.fromString(idString);
+      private void printTodos(List<Todo> todos) {
+            if (todos.isEmpty()) {
+                System.out.println("No tasks found.");
+            } else {
+                System.out.println("Tasks:");
+                for (Todo todo : todos) {
+                    System.out.println(todo);
+                }
+            }
+      }
 
-            Todo todo= Todo.builder()
-                    .id(id)
-                    .build();
-        todoService.deleteTask(todo);
+
+
+        private void updateTask() {
+            try {
+                System.out.println("Enter the ID of the task you want to update:");
+                String idString = scanner.nextLine();
+                UUID id = UUID.fromString(idString);
+
+                // Gerekli bilgileri kullanıcıdan alabilirsiniz
+                System.out.println("Enter new title:");
+                String newTitle = scanner.nextLine();
+                System.out.println("Enter new description:");
+                String newDescription = scanner.nextLine();
+                System.out.println("Enter new status:");
+                String newStatus = scanner.nextLine();
+
+                Todo todo = Todo.builder()
+                        .id(id)
+                        .title(newTitle)
+                        .description(newDescription)
+                        .status(Status.valueOf(newStatus))
+                        .build();
+
+                todoService.updateTask(todo);
+                System.out.println("Task updated successfully.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+
+
+
+
+        private void deleteTask() {
+            try {
+                System.out.println("Enter the ID of the task you want to delete:");
+                String idString = scanner.nextLine();
+                UUID id = UUID.fromString(idString);
+
+                Todo todoToDelete = Todo.builder()
+                        .id(id)
+                        .build();
+
+                todoService.deleteTodo(todoToDelete);
+                System.out.println("Task deleted successfully.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+
+
 
     }
 }
