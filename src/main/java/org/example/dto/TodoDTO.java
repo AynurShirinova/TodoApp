3aeb0517d940fc86ca1709ab855e014989aa7c90
Aynur Sirinova <wirinovaaynur123@gmail.com>
2024-04-03 01:22:14 +0400

    Create TodoDTO.java

---
+++ src/main/java/org/example/dto/TodoDTO.java
@@ -0,0 +1,27 @@
+package org.example.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.example.domain.Status;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class TodoDTO {
+    private UUID id;
+    private String projectId;
+    private String title;
+    private String description;
+    private String createdBy;
+    private UUID assignedTo;
+    private Status status;
+    private String priority;
+    private LocalDate created;
+
+
+}
