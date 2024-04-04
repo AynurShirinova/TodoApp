4a0637c4d6b3ebfa4dfbe82e570e8a93cb5ff5db
Aynur Sirinova <wirinovaaynur123@gmail.com>
2024-04-04 22:52:45 +0400

    Create UsersDTO.java

---
+++ src/main/java/org/example/dto/UsersDTO.java
@@ -0,0 +1,19 @@
+package org.example.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.example.domain.Priority;
+import org.example.domain.Status;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UsersDTO {
+    UUID id;
+    String userName;
+}
